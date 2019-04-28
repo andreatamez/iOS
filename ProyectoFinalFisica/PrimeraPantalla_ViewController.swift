1d4e52e6e40bfd63f7e6b0733504517cbfa46d8a
alejandrolozanoz <alejandrolozanoz96@gmail.com>
2019-04-28 18:03:45 -0500

    Make Quiz Work with Tabbed App

---
+++ ProyectoFinalFisica/PrimeraPantalla_ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PrimeraPantalla_ViewController.swift
+//  ProyectoFinalFisica
+//
+//  Created by Andrea Tamez on 4/1/19.
+//  Copyright © 2019 Andrea Tamez. All rights reserved.
+//
+
+import UIKit
+
+class PrimeraPantalla_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
